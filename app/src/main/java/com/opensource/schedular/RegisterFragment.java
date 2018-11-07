bb0af31569f6fc370d61d558dceaeec9f7b5a4c4
Achala Kavinda <achalakavinda25r@gmail.com>
2018-11-07 18:42:09 +0530

    firebase add

--- app/src/main/java/com/opensource/schedular/RegisterFragment.java
+++ app/src/main/java/com/opensource/schedular/RegisterFragment.java
@@ -2,14 +2,24 @@ package com.opensource.schedular;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import util.DialogBox;
 
 
 /**
@@ -17,14 +27,13 @@ import android.widget.EditText;
  */
 public class RegisterFragment extends Fragment implements View.OnClickListener {
 
-    final MainActivity mainActivity = (MainActivity) getActivity();
-    private static final String TAG = "LoginFragment";
+    private static final String TAG = "RegisterFragment";
     private String error = null;
 
-
-
+    private FirebaseAuth mAuth;
 
     private View view = null;
+
 
     private EditText editTextName;
     private EditText editTextDeviceID;
@@ -37,6 +46,7 @@ public class RegisterFragment extends Fragment implements View.OnClickListener {
     @Override
     public void onStart() {
         super.onStart();
+        mAuth = FirebaseAuth.getInstance();
     }
 
     @Override
@@ -93,7 +103,7 @@ public class RegisterFragment extends Fragment implements View.OnClickListener {
 
             case R.id.registerBtn:
                 // new
-                registerFn();
+                register("achalakavinda25r@gmail.com","admin123");
                 break;
             default:
                 break;
@@ -101,8 +111,28 @@ public class RegisterFragment extends Fragment implements View.OnClickListener {
     }
 
 
-    public void registerFn() {
+    public void register(String email,String password){
 
+        Log.d(TAG,"Register Function call");
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "createUserWithEmail:success");
+                            Toast.makeText(getActivity().getApplicationContext(),"User Create",Toast.LENGTH_LONG).show();
+                            FirebaseUser user = mAuth.getCurrentUser();
+//                            insertUSerData(user.getUid());
+                        } else {
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            DialogBox dBox = new DialogBox();
+                            dBox.ViewDialogBox(view,"Wrong!",task.getException().getMessage());
+                        }
+
+                    }
+                });
     }
+
 
 }
