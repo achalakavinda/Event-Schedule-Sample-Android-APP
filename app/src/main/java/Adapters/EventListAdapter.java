5a86388da8315c83c93d5835ebaf89d5031ca062
AchalaKavinda <achalakavinda25r@gmail.com>
2018-11-09 01:27:54 +0530

    add recycler view and animation

---
+++ app/src/main/java/Adapters/EventListAdapter.java
@@ -0,0 +1,134 @@
+package Adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.opensource.schedular.R;
+
+import java.util.List;
+
+import Model.EventModel;
+
+
+
+public class EventListAdapter extends RecyclerView.Adapter<EventListAdapter.ViewHolder> {
+
+    private List<EventModel> values;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+
+        public View layout;
+
+
+
+        //custom view variables
+        public LinearLayout linearLayoutPostItemCard;
+
+        public ViewHolder(View v) {
+            super(v);
+            layout = v;
+
+            //custom view variable initialize
+            linearLayoutPostItemCard = (LinearLayout) v.findViewById(R.id.EventItemCard);
+        }
+    }
+
+
+
+    public void add(int position, EventModel item) {
+        values.add(position, item);
+        notifyItemInserted(position);
+    }
+
+    public void remove(int position) {
+        values.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public EventListAdapter(List<EventModel> myDataset) {
+        values = myDataset;
+    }
+
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public EventListAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                             int viewType) {
+        // create a new view
+        LayoutInflater inflater = LayoutInflater.from(
+                parent.getContext());
+        View v = inflater.inflate(R.layout.recycler_event_item, parent, false);
+
+
+
+        // set the view's size, margins, paddings and layout parameters
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    private int lastPosition = -1;
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, final int position) {
+
+
+//        holder.textViewName.setText(values.get(position).getUsername().toString());
+
+
+        Animation animation = AnimationUtils.loadAnimation(holder.itemView.getContext(), (position > lastPosition) ? R.anim.up_from_bottom : R.anim.down_from_top);
+        holder.itemView.startAnimation(animation);
+        lastPosition = position;
+
+
+        holder.linearLayoutPostItemCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                remove(position);
+//                switch (v.getId()){
+//
+//                    case R.id.postItemCard:
+//                        Context context = v.getContext();
+//                        Intent intent = new Intent(context,SinglePostActivity.class);
+//
+//                        intent.putExtra("Name",values.get(position).getName().toString());
+//                        intent.putExtra("Date",values.get(position).getDate().toString());
+//                        intent.putExtra("AvatarImageUrl",values.get(position).getAvatarImageUrl().toString());
+//                        intent.putExtra("Description",values.get(position).getDescription().toString());
+//                        intent.putExtra("Likes",values.get(position).getLikes().toString());
+//                        intent.putExtra("Id",values.get(position).getId().toString());
+//                        intent.putExtra("user_id",values.get(position).getUser_id().toString());
+//                        intent.putExtra("username",values.get(position).getUsername().toString());
+//
+//
+//                        context.startActivity(intent);
+//                        break;
+//                    default:
+//                        return;
+//                }
+            }
+        });
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return values.size();
+    }
+
+
+}
--- app/src/main/java/com/opensource/schedular/EventListActivity.java
+++ app/src/main/java/com/opensource/schedular/EventListActivity.java
@@ -2,12 +2,19 @@ package com.opensource.schedular;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
+
+import Adapters.EventListAdapter;
+import Model.EventModel;
 
 public class EventListActivity extends Activity implements View.OnClickListener{
 
@@ -15,7 +22,14 @@ public class EventListActivity extends Activity implements View.OnClickListener{
     private TextView time;
     private Calendar calendar;
     private String format = "";
-    private Button buttonSet;
+
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+
+    List<EventModel> input = new ArrayList<>();
 
 
 
@@ -25,15 +39,34 @@ public class EventListActivity extends Activity implements View.OnClickListener{
         setContentView(R.layout.activity_event_list);
         timePicker1 = (TimePicker) findViewById(R.id.timePicker1);
 
-        time = (TextView) findViewById(R.id.textView1);
+//        time = (TextView) findViewById(R.id.textView1);
         calendar = Calendar.getInstance();
 
-        buttonSet = findViewById(R.id.set_button);
-        buttonSet.setOnClickListener(this);
 
         int hour = calendar.get(Calendar.HOUR_OF_DAY);
         int min = calendar.get(Calendar.MINUTE);
         showTime(hour, min);
+
+        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        recyclerView.setHasFixedSize(true);
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        input.add(new EventModel());
+        input.add(new EventModel());
+        input.add(new EventModel());
+        input.add(new EventModel());
+        input.add(new EventModel());
+        input.add(new EventModel());
+        input.add(new EventModel());
+        input.add(new EventModel());
+        input.add(new EventModel());
+        input.add(new EventModel());
+        input.add(new EventModel());
+
+        mAdapter = new EventListAdapter(input);
+        recyclerView.setAdapter(mAdapter);
     }
 
     public void showTime(int hour, int min) {
@@ -49,23 +82,23 @@ public class EventListActivity extends Activity implements View.OnClickListener{
             format = "AM";
         }
 
-        time.setText(new StringBuilder().append(hour).append(" : ").append(min)
-                .append(" ").append(format));
+//        time.setText(new StringBuilder().append(hour).append(" : ").append(min)
+//                .append(" ").append(format));
     }
 
     @Override
     public void onClick(View v) {
 
-        switch (v.getId())
-        {
-            case R.id.set_button:
-                int hour = calendar.get(Calendar.HOUR_OF_DAY);
-                int min = calendar.get(Calendar.MINUTE);
-                showTime(hour, min);
-                break;
-                default:
-                return;
-        }
+//        switch (v.getId())
+//        {
+//            case R.id.set_button:
+//                int hour = calendar.get(Calendar.HOUR_OF_DAY);
+//                int min = calendar.get(Calendar.MINUTE);
+//                showTime(hour, min);
+//                break;
+//                default:
+//                return;
+//        }
 
     }
 }
